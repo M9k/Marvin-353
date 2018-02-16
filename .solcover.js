f8a9a29e4d6440f31683a0f1fe75221787b839cb
maztek <marraffa3ai@gmail.com>
2018-02-16 12:37:02 +0100

    Rimossi test solidity da test di react e rimosso coverage per i file di test

--- .solcover.js
+++ .solcover.js
@@ -1,6 +1,6 @@
 module.exports = {
   norpc: true,
   compileCommand: '../node_modules/.bin/truffle compile --network coverage',
-  testCommand: '../node_modules/.bin/truffle test test/contracts/* --network coverage',
+  testCommand: '../node_modules/.bin/truffle test test/_contracts/* --network coverage',
   port: 8545
 };
