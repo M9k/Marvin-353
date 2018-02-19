e4159a867ff74ebd438d850f08b2edd82d8ae478
Mirco <mcailotto96@gmail.com>
2018-02-19 16:47:43 +0100

    Aggiunto button factory in base all'utente

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import Button from './buttons/Button';
-import ButtonPrice from './buttons/ButtonPrice';
-import ButtonHelp from './buttons/ButtonHelp';
 import Header from './global/Header';
+import Button from './buttons/Button';
+import ButtonFactory from './buttons/ButtonFactory';
+import AccountTypes from './AccountEnum';
 
 // Home page component
 // TODO: Cambiare la home in base se l'utente è già autenticato nel sistema
-const Home = () => (
-  <div className="page-home">
-    <Header>
-      <ButtonPrice />
-      <ButtonHelp />
-    </Header>
-    <div className="page-content">
-      <Button link="/login">Login</Button><br />
-      <Button link="/register">Register</Button><br />
-      <h2>Metamask installato: {typeof web3 !== 'undefined' ? 'SI' : 'NO'}</h2><br />
-      <h2>Metamask sbloccato: {typeof web3 !== 'undefined' && typeof web3.eth.accounts[0] !== 'undefined' ? 'SI' : 'NO'}</h2><br />
-      <h2>Versione di Web3: {typeof web3 !== 'undefined' && web3 !== null ? web3.version.api : 'NOT FOUND!!'}</h2><br />
-      <h2>Account in uso: {typeof web3 !== 'undefined' && typeof web3.eth.accounts[0] !== 'undefined' ? web3.eth.accounts[0] : 'NOT FOUND!!'}</h2><br />
+const Home = (props) => {
+  document.title = 'Home - Marvin';
+  // TODO: Prelevare il tipo di account da Redux
+  return (
+    <div className="page-home">
+      <Header>
+        <ButtonFactory AccountType={AccountTypes.NOTLOGGED} />
+      </Header>
+      <div className="page-content">
+        <Button link="/login">Login</Button><br />
+        <Button link="/register">Register</Button><br />
+        <h2>Metamask installato: {typeof web3 !== 'undefined' ? 'SI' : 'NO'}</h2><br />
+        <h2>Metamask sbloccato: {typeof web3 !== 'undefined' && typeof web3.eth.accounts[0] !== 'undefined' ? 'SI' : 'NO'}</h2><br />
+        <h2>Versione di Web3: {typeof web3 !== 'undefined' && web3 !== null ? web3.version.api : 'NOT FOUND!!'}</h2><br />
+        <h2>Account in uso: {typeof web3 !== 'undefined' && typeof web3.eth.accounts[0] !== 'undefined' ? web3.eth.accounts[0] : 'NOT FOUND!!'}</h2><br />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Home;
---
+++ src/components/buttons/ButtonFactory.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ButtonPrice from './ButtonPrice';
+import ButtonHelp from './ButtonHelp';
+import ButtonLogOut from './ButtonLogOut';
+import AccountTypes from '../AccountEnum';
+
+const ButtonFactory = (props) => {
+  switch (props.AccountType) {
+    case AccountTypes.NOTLOGGED:
+      return (
+        <div id="ButtonGroup">
+          <ButtonPrice />
+          <ButtonHelp />
+        </div>
+      );
+    case AccountTypes.UNIVERSITY:
+    case AccountTypes.ADMIN:
+    case AccountTypes.PROFESSOR:
+    case AccountTypes.STUDENT:
+      return (
+        <div id="ButtonGroup">
+          <ButtonPrice />
+          <ButtonHelp />
+          <ButtonLogOut />
+        </div>
+      );
+    default:
+      return (
+        <div id="ButtonGroup" />
+      );
+  }
+};
+
+ButtonFactory.ButtonFactory = {
+  AccountType: PropTypes.oneOf(AccountTypes).isRequired,
+};
+
+export default ButtonFactory;
+
