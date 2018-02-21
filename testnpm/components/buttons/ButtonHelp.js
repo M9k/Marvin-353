5f39c4432d2acf2a9b6ccfda3d2835c5a423c2e3
Mirco <mcailotto96@gmail.com>
2018-02-21 01:47:07 +0100

    Fixati i test presenti, da estendere

--- testnpm/components/buttons/ButtonHelp.js
+++ testnpm/components/buttons/ButtonHelp.js
@@ -1,24 +1,40 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import assert from 'assert';
+import configureStore from 'redux-mock-store';
 import ButtonHelp from '../../../src/components/buttons/ButtonHelp';
-
 
 // unit tests for the App component
 describe('ButtonHelp component', () => {
-  //mock
+  // mock
   const Button = props => (<div>{props.link} {props.children}</div>);
+  const mockStore = configureStore();
   describe('render()', () => {
     it('should render the component', () => {
-      const wrapper = shallow(<ButtonHelp />);
+      const wrapper = shallow(<ButtonHelp
+        store={mockStore({
+          routing:
+            { locationBeforeTransitions: { pathname: 'test' } },
+          })}
+      />);
       assert.equal(wrapper.length, 1);
     });
     it('should indicate the help page', () => {
-      const wrapper = shallow(<ButtonHelp />);
+      const wrapper = shallow(<ButtonHelp
+        store={mockStore({
+          routing:
+            { locationBeforeTransitions: { pathname: 'test' } },
+        })}
+      />);
       assert.equal(wrapper.html().search('/help') !== -1, true);
     });
     it('should have "Help" as text', () => {
-      const wrapper = shallow(<ButtonHelp />);
+      const wrapper = shallow(<ButtonHelp
+        store={mockStore({
+          routing:
+            { locationBeforeTransitions: { pathname: 'test' } },
+        })}
+      />);
       assert.equal(wrapper.html().search('Help') !== -1, true);
     });
   });
--- testnpm/components/buttons/ButtonPrice.js
+++ testnpm/components/buttons/ButtonPrice.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import assert from 'assert';
+import configureStore from 'redux-mock-store';
 import ButtonPrice from '../../../src/components/buttons/ButtonPrice';
 
 
@@ -8,17 +9,33 @@ import ButtonPrice from '../../../src/components/buttons/ButtonPrice';
 describe('ButtonPrice component', () => {
   //mock
   const Button = props => (<div>{props.link} {props.children}</div>);
+  const mockStore = configureStore();
   describe('render()', () => {
     it('should render the component', () => {
-      const wrapper = shallow(<ButtonPrice />);
+      const wrapper = shallow(<ButtonPrice
+        store={mockStore({
+          routing:
+            { locationBeforeTransitions: { pathname: 'test' } },
+        })}
+      />);
       assert.equal(wrapper.length, 1);
     });
     it('should indicate the price page', () => {
-      const wrapper = shallow(<ButtonPrice />);
+      const wrapper = shallow(<ButtonPrice
+        store={mockStore({
+          routing:
+            { locationBeforeTransitions: { pathname: 'test' } },
+        })}
+      />);
       assert.equal(wrapper.html().search('/price') !== -1, true);
     });
     it('should have "Price" as text', () => {
-      const wrapper = shallow(<ButtonPrice />);
+      const wrapper = shallow(<ButtonPrice
+        store={mockStore({
+          routing:
+            { locationBeforeTransitions: { pathname: 'test' } },
+        })}
+      />);
       assert.equal(wrapper.html().search('Price') !== -1, true);
     });
   });
