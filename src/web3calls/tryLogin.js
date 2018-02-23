f22d318add9c562dd3eb68f035cd55f5f36da6df
giorat <giorat.96@gmail.com>
2018-02-23 17:29:56 +0100

    current account from metamask

--- src/web3calls/tryLogin.js
+++ src/web3calls/tryLogin.js
@@ -4,20 +4,17 @@
 const contract = require('truffle-contract');
 
 function funzioneTest() {
-  let returnType = 0;
+  const returnType = 0;
   // TODO: da spostare
   const contractUniversityJson = require('../../build/contracts/University.json');
 
   const contractUniversity = contract(contractUniversityJson);
   contractUniversity.setProvider(web3.currentProvider);
 
-  return contractUniversity.deployed().then((instance) => {
+  return contractUniversity.deployed().then(instance =>
     // Attempt to login user.
-    return instance.login();
-  }).then((result) => {
-    return result;
-  });
-  //return returnType;
+    instance.login.call({ from: web3.eth.accounts[0] })).then(result => result);
+  // return returnType;
 }
 
 export default funzioneTest;
