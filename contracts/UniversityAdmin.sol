65675eb6f7f17a13010f122d0162fdd0dbe574e1
Mirco <mcailotto96@gmail.com>
2018-02-26 21:06:32 +0100

    Aggiunta cancellazione admin

--- contracts/UniversityAdmin.sol
+++ contracts/UniversityAdmin.sol
@@ -3,32 +3,57 @@ import "./UniversityBase.sol";
 
 
 contract UniversityAdmin is UniversityBase {
-    uint private countAdministrators;
-    mapping (address => bool) private administrators;
+    // zero = not found
+    uint private countAdministrators = 1;
+    mapping (address => uint) private administrators;
     mapping (uint => address) private administratorsByIndex;
 
+    modifier onlyAdmin {
+        require(administrators[msg.sender] != 0);
+        _;
+    }
+
+    modifier validAdminIndex(uint _index) {
+        require(_index < countAdministrators && _index != 0);
+        _;
+    }
+
+    modifier validAdminAddress(address _address) {
+        require(administrators[_address] != 0);
+        _;
+    }
+
     //add an admin
-    function newAdmin(address _adminAddress)  public onlyFounder
+    function newAdmin(address _adminAddress) public onlyFounder
     registrableAddress(_adminAddress) {
         registered[_adminAddress] = true;
-        administrators[_adminAddress] = true;
+        administrators[_adminAddress] = countAdministrators;
         administratorsByIndex[countAdministrators] = _adminAddress;
         countAdministrators += 1;
     }
 
     //Check if a _adminAddress is an admin
     function isAdmin(address _adminAddress) public view returns(bool) {
-        return administrators[_adminAddress];
+        return administrators[_adminAddress] != 0;
     }
 
     //Return the number of admins
     function getAdminsNumber() public view returns(uint) {
-        return countAdministrators;
+        return countAdministrators - 1;
     }
 
     //return the admin ad index _index
-    function getAdminAt(uint _index) public view returns(address) {
+    function getAdminAt(uint _index) public view validAdminIndex(_index) returns(address) {
         return administratorsByIndex[_index];
+    }
+    //get index of admin with address of not usefull
+
+    //remove the admin with address _address
+    function removeAdmin(address _address) public onlyFounder validAdminAddress(_address) {
+        registered[_address] = false;
+        administratorsByIndex[administrators[_address]] = administratorsByIndex[countAdministrators];
+        administrators[_address] = 0;
+        countAdministrators -= 1;
     }
 
     function login() public view returns (uint typeUser) {
