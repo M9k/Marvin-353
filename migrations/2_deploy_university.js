81db5efaf131caada07a38eebc64bd342d617ddd
Mirco <mcailotto96@gmail.com>
2018-02-27 21:33:19 +0100

    Fix deploy

--- migrations/2_deploy_university.js
+++ migrations/2_deploy_university.js
@@ -1,13 +1,6 @@
-const Student = artifacts.require('./Student.sol');
-
-const UniversityBase = artifacts.require('./UniversityBase.sol');
-
 const UniversityAdmin = artifacts.require('./UniversityAdmin.sol');
 
 module.exports = function (deployer) {
-  deployer.deploy(Student, 'mario', 'rossi');
-
-  deployer.deploy(UniversityBase);
-  deployer.link(UniversityBase, UniversityAdmin);
+  //deployer.link(UniversityBase, UniversityAdmin);
   deployer.deploy(UniversityAdmin);
 };
