68780c3cfc42b8b4a9e745390d26cdb35a374012
Parwinder Singh <33651643+SinghParwinder@users.noreply.github.com>
2018-02-27 16:47:14 +0100

    intestazioni contratto student

--- contracts/Student.sol
+++ contracts/Student.sol
@@ -1,16 +1,16 @@
 pragma solidity ^0.4.2;
 
+
 contract Student {
-  address public creator;
-  bytes32 public name;
-  bytes32 public surname;
-  bool public registered;
+    address public creator;
+    bytes32 public name;
+    bytes32 public surname;
+    bool public registered;
 
-  function Student(bytes32 _name, bytes32 _surname) public {
-    creator = msg.sender;
-    name = _name;
-    surname = _surname;
-    registered = false;
-  }
-
+    function Student(bytes32 _name, bytes32 _surname) public {
+        creator = msg.sender;
+        name = _name;
+        surname = _surname;
+        registered = false;
+    }
 }
