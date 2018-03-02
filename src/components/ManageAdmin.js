a49478ebeba07d91648a174ea44451ad338ac53c
Mirco <mcailotto96@gmail.com>
2018-03-02 16:16:06 +0100

    Spostata form aggiunta admin

--- src/components/ManageAdmin.js
+++ src/components/ManageAdmin.js
@@ -1,36 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { FormGroup, ControlLabel, FormControl, Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import Header from './global/Header';
 import { universityAction } from '../reducers/university';
+import FormAddAdmin from './form/FormAddAdmin';
 
 class ManageAdmin extends React.Component {
   constructor(props) {
     super(props);
     document.title = 'Manage Admin - Marvin';
-
-    this.state = {
-      newAddress: '',
-    };
-    this.handleAddressChange = this.handleAddressChange.bind(this);
   }
   componentWillMount() {
     this.props.getAdminNumber();
-  }
-
-  getValidationState() {
-    if (this.state.newAddress.length === 0) {
-      return 'warning';
-    }
-    if (!/^(0x)[0-9a-f]{40}$/i.test(this.state.newAddress) || !web3.isAddress(this.state.newAddress)) {
-      return 'error';
-    }
-    return 'success';
-  }
-
-  handleAddressChange(e) {
-    this.setState({ newAddress: e.target.value });
   }
 
   render() {
@@ -41,16 +23,7 @@ class ManageAdmin extends React.Component {
           <Button onClick={this.props.getAdminNumber}>Admins number:</Button>
          ={this.props.adminNumber}<br />
         </div>
-        <FormGroup validationState={this.getValidationState()}>
-          <ControlLabel>New admin address:</ControlLabel>
-          <FormControl
-            id="formControlsText"
-            type="text"
-            placeholder="0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf"
-            onChange={this.handleAddressChange}
-          />
-          {this.getValidationState() === 'success' ? <Button onClick={() => this.props.addAdmin(this.state.newAddress)}>Add</Button> : <Button disabled>Add</Button>}
-        </FormGroup>
+        <FormAddAdmin addAdmin={this.props.addAdmin} />
       </div>
     );
   }
