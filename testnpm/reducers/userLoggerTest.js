467b917c873e1bd4829bc11347c2c9525ca4e885
ValeMarcon <vavi1996@yahoo.it>
2018-03-02 17:29:53 +0100

    test aggiuntivi da espandere

--- testnpm/reducers/userLoggerTest.js
+++ testnpm/reducers/userLoggerTest.js
@@ -1,12 +1,25 @@
-import * as types from '../../src/actions/actions.js';
-import { userLogger } from '../../src/reducers/user';
-import { initialState } from '../../src/reducers/user.js';
-
-//const assert = require('chai').assert;
-const expect = require('chai').expect;
+import { expect } from 'chai';
+import * as types from '../../src/actions/actions';
+import { userLogger, initialState } from '../../src/reducers/user';
 
 describe('userLogger reducer', () => {
   it('should have initial state', () => {
     expect(userLogger(undefined, {})).to.deep.equal(initialState);
+  });
+
+  it('should say metamask is true', () => {
+    const action = { type: types.userAction.METAMASK };
+    expect(userLogger({}, action)).to.deep.equal({
+      metamask: true,
+    });
+  });
+
+  it('should say user is logged', () => {
+    const action = { type: types.userAction.USER_LOGGED_IN };
+    expect(userLogger({}, action)).to.deep.equal({
+      trylogin: false,
+      logged: true,
+      role: undefined,
+    });
   });
 });
