d422796e7d4c5d1c82f164de96baaa6b9cf062ee
Mirco <mcailotto96@gmail.com>
2018-03-02 17:15:32 +0100

    Fix ESLint

--- src/components/load/logging.js
+++ src/components/load/logging.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';;
+import PropTypes from 'prop-types';
 import { userAction } from '../../actions/actions';
 import RedirectToHome from './RedirectToHome';
 
