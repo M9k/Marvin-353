8358c4366aec8c2c3abc8df85b1600cd157e4a9c
Mirco <mcailotto96@gmail.com>
2018-03-03 16:37:03 +0100

    Corretto errore ESLint all'interno dei test Truffle

--- test/UniversityTeacher.js
+++ test/UniversityTeacher.js
@@ -69,12 +69,14 @@ contract('UniversityTeacher', (accounts) => {
     assert.equal(await contract.getTeachersNumber.call({ from: accounts[0] }), 1);
     assert.equal(await contract.getUnconfirmedTeachersNumber.call({ from: accounts[0] }), 1);
   });
-
-  // Testing get functions 
-  it(testTitle('Should return correct teacher and unconfirmed teacher with given index!'), async () => {
-    assert.equal(await contract.getTeacherAtIndex.call(0), accounts[1]);
-    assert.equal(await contract.getUnconfirmedTeacherAtIndex.call(0), accounts[4]);
-  });
+  // Testing get functions
+  it(
+    testTitle('Should return correct teacher and unconfirmed teacher with given index!'),
+    async () => {
+      assert.equal(await contract.getTeacherAtIndex.call(0), accounts[1]);
+      assert.equal(await contract.getUnconfirmedTeacherAtIndex.call(0), accounts[4]);
+    },
+  );
 
   // Testing login function
   it(testTitle('Should login with university, teacher and unconfirmed accounts!'), async () => {
