fc215cf83a5b31993bdcf5ae0b24405b7c7aae9f
giorat <giorat.96@gmail.com>
2018-03-10 09:55:38 +0100

    :recycle: rimosso ganache

--- truffle-config.js
+++ truffle-config.js
@@ -10,11 +10,6 @@ module.exports = {
       port: 8545,
       network_id: '5777',
     },
-    ganache: {
-      host: '127.0.0.1',
-      port: 7545,
-      network_id: '5777',
-    },
     coverage: {
       /* Per il coverage, ma anche per i test in generale, potremmo usare testrpc (che usa ganache)
        per fissare una mnemonica in modo da avere sempre gli stessi account su cui fare i test"
