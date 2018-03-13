c0305da8bd0753322c05305f356d51fbdf68b6f3
giorat <giorat.96@gmail.com>
2018-03-13 21:59:24 +0100

    fix testrpc win account e switch test

--- src/reducers/university.js
+++ src/reducers/university.js
@@ -7,26 +7,24 @@ const initialState = {
 
 const universityData = (state = initialState, action) => {
   // reset
-  if (action.type === universityAction.GET_ADMIN_NUMBER) {
-    return Object.assign({}, state, {});
-  }
-  if (action.type === universityAction.RETURN_ADMIN_NUMBER) {
-    return Object.assign({}, state, {
-      adminNumber: action.adminNumber,
-    });
-  }
-  if (action.type === universityAction.ADD_NEW_ADMIN) {
-    return Object.assign({}, state, {});
-  }
-  if (action.type === universityAction.GET_ALL_ADMINS) {
-    return Object.assign({}, state, {});
-  }
-  if (action.type === universityAction.RETURN_ALL_ADMINS) {
-    return Object.assign({}, state, {
-      adminAccount: action.account,
-    });
-  }
-  return state;
+  switch (action.type) {
+    case universityAction.GET_ADMIN_NUMBER:
+      return Object.assign({}, state, {});
+    case universityAction.RETURN_ADMIN_NUMBER:
+      return Object.assign({}, state, {
+        adminNumber: action.adminNumber,
+      });
+    case universityAction.ADD_NEW_ADMIN:
+      return Object.assign({}, state, {});
+    case universityAction.GET_ALL_ADMINS:
+      return Object.assign({}, state, {});
+    case universityAction.RETURN_ALL_ADMINS:
+      return Object.assign({}, state, {
+        adminAccount: action.account,
+      });
+    default:
+      return state;
+  }// switch
 };
 
 export { universityData, initialState };
