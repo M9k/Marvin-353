b176d2d847a343e4b593f2302e2613ab9c6cee73
Mirco <mcailotto96@gmail.com>
2018-03-13 23:45:18 +0100

    Swtich reducer utente

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -9,44 +9,43 @@ const initialState = {
 };
 
 const userLogger = (state = initialState, action) => {
-  if (action.type === userAction.USER_LOGGED_IN || action.type === userAction.USER_UPDATED) {
-    return Object.assign({}, state, {
-      trylogin: false,
-      logged: true,
-      role: action.role,
-    });
-  }
-  if (action.type === userAction.USER_TRY_LOGIN) {
-    return Object.assign({}, state, {
-      trylogin: true,
-      logged: false,
-      role: null,
-    });
-  }
-  if (action.type === userAction.METAMASK_NOT_FOUND) {
-    return Object.assign({}, state, {
-      metamask: false,
-    });
-  }
-  if (action.type === userAction.EDIT_ADDRESS) {
-    if (typeof action.address !== 'undefined') {
+  switch (action.type) {
+    case (userAction.USER_LOGGED_IN):
+    case (userAction.USER_UPDATED):
       return Object.assign({}, state, {
-        account: action.address,
+        trylogin: false,
+        logged: true,
+        role: action.role,
       });
-    }
-    return Object.assign({}, state, {
-      account: '',
-    });
+    case (userAction.USER_TRY_LOGIN):
+      return Object.assign({}, state, {
+        trylogin: true,
+        logged: false,
+        role: null,
+      });
+    case (userAction.METAMASK_NOT_FOUND):
+      return Object.assign({}, state, {
+        metamask: false,
+      });
+    case (userAction.EDIT_ADDRESS):
+      if (typeof action.address !== 'undefined') {
+        return Object.assign({}, state, {
+          account: action.address,
+        });
+      } else {
+        return Object.assign({}, state, {
+          account: '',
+        });
+      }
+    case (userAction.USER_LOGGED_OUT):
+      return Object.assign({}, state, {
+        trylogin: false,
+        logged: false,
+        role: null,
+      });
+    default:
+      return state;
   }
-  if (action.type === userAction.USER_LOGGED_OUT) {
-    return Object.assign({}, state, {
-      trylogin: false,
-      logged: false,
-      role: null,
-    });
-  }
-
-  return state;
 };
 
 export { userLogger, initialState };
