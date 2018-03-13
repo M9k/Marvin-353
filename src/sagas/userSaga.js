19a4f47eb08ecf9a1137c677e5c45a28e3f078bd
Mirco <mcailotto96@gmail.com>
2018-03-13 23:39:03 +0100

    Rimosso messaggio di debug

--- src/sagas/userSaga.js
+++ src/sagas/userSaga.js
@@ -4,9 +4,7 @@ import login from '../web3calls/login';
 
 export function* tryLogin() {
   // call the web3call for try to login
-  console.log('A');
   const userType = yield call(login);
-  console.log('B');
   console.log('You are a: ');
   console.log(Number(userType));
   // update the user state via reducer
