472ef118506861f2b17641c3a8d383341cfc6749
Mirco <mcailotto96@gmail.com>
2018-03-13 23:39:17 +0100

    Reinserito switch

--- src/reducers/university.js
+++ src/reducers/university.js
@@ -6,27 +6,24 @@ const initialState = {
 };
 
 const universityData = (state = initialState, action) => {
-  // reset
-  if (action.type === universityAction.GET_ADMIN_NUMBER) {
-    return Object.assign({}, state, {});
+  switch (action.type) {
+    case (universityAction.GET_ADMIN_NUMBER):
+      return state;
+    case (action.type === universityAction.SET_ADMIN_NUMBER):
+      return Object.assign({}, state, {
+        adminNumber: action.adminNumber,
+      });
+    case (action.type === universityAction.ADD_NEW_ADMIN):
+      return state;
+    case (action.type === universityAction.GET_ALL_ADMINS):
+      return state;
+    case (action.type === universityAction.SET_ADMINS_LIST):
+      return Object.assign({}, state, {
+        adminAccount: action.account,
+      });
+    default:
+      return state;
   }
-  if (action.type === universityAction.SET_ADMIN_NUMBER) {
-    return Object.assign({}, state, {
-      adminNumber: action.adminNumber,
-    });
-  }
-  if (action.type === universityAction.ADD_NEW_ADMIN) {
-    return Object.assign({}, state, {});
-  }
-  if (action.type === universityAction.GET_ALL_ADMINS) {
-    return Object.assign({}, state, {});
-  }
-  if (action.type === universityAction.SET_ADMINS_LIST) {
-    return Object.assign({}, state, {
-      adminAccount: action.account,
-    });
-  }
-  return state;
 };
 
 export { universityData, initialState };
