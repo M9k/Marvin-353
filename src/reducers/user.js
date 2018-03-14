2cc13cb94c003c1f19493f1258008287a2826111
giorat <giorat.96@gmail.com>
2018-03-14 08:49:12 +0100

    :recycle: rimosso truffle windows e prova reducer con es6

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -12,36 +12,42 @@ const userLogger = (state = initialState, action) => {
   switch (action.type) {
     case (userAction.USER_LOGGED_IN):
     case (userAction.USER_UPDATED):
-      return Object.assign({}, state, {
+      return {
+        ...state,
         trylogin: false,
         logged: true,
         role: action.role,
-      });
+      };
     case (userAction.USER_TRY_LOGIN):
-      return Object.assign({}, state, {
+      return {
+        ...state,
         trylogin: true,
         logged: false,
         role: null,
-      });
+      };
     case (userAction.METAMASK_NOT_FOUND):
-      return Object.assign({}, state, {
+      return {
+        ...state,
         metamask: false,
-      });
+      };
     case (userAction.EDIT_ADDRESS):
       if (typeof action.address !== 'undefined') {
-        return Object.assign({}, state, {
+        return {
+          ...state,
           account: action.address,
-        });
+        };
       }
-      return Object.assign({}, state, {
+      return {
+        ...state,
         account: '',
-      });
+      };
     case (userAction.USER_LOGGED_OUT):
-      return Object.assign({}, state, {
+      return {
+        ...state,
         trylogin: false,
         logged: false,
         role: null,
-      });
+      };
     default:
       return state;
   }
