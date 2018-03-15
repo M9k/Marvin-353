14f7c81bd97dd1e19179af37403b6c87a71e481b
gmarraffa <marraffa3ai@gmail.com>
2018-03-15 11:21:32 +0100

    tolto limite non capisco

--- migrations/2_deploy_university.js
+++ migrations/2_deploy_university.js
@@ -7,10 +7,10 @@ module.exports = function (deployer) {
   deployer.deploy(User, 'mario', 'rossi');
 
   // ONLY FOR SOLTEST! NOT FOR DEPLOY
-  deployer.deploy(UniversityBase, {gas: 0xfffffffffff});
+  deployer.deploy(UniversityBase);
   // ONLY FOR SOLTEST! NOT FOR DEPLOY
   deployer.link(UniversityBase, UniversityAdmin);
-  deployer.deploy(UniversityAdmin, {gas: 0xfffffffffff});
+  deployer.deploy(UniversityAdmin);
   deployer.link(UniversityAdmin, UniversityTeacher);
-  deployer.deploy(UniversityTeacher, {gas: 0xfffffffffff});
+  deployer.deploy(UniversityTeacher);
 };
