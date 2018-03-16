c5bd1eb30f027b9b6b4ede4c6244b130f186e4a4
gmarraffa <marraffa3ai@gmail.com>
2018-03-16 17:33:18 +0100

    Modificati dettagli build fallita e prova errore in before_script

--- contracts/Teacher.sol
+++ contracts/Teacher.sol
@@ -3,6 +3,7 @@ import "./User.sol";
 
 
 contract Teacher is User {
-    function Teacher(bytes32 _name, bytes32 _surname) public User(_name, _surname) {
+    function Teacher(bytes32
+      _name, bytes32 _surname) public User(_name, _surname) {
     }
 }
