cddd88288cdf17ab7d99f988c691003e8565fed3
gmarraffa <marraffa3ai@gmail.com>
2018-03-16 17:51:45 +0100

    Sistemato errore volontario e per oggi basta notifiche

--- contracts/Teacher.sol
+++ contracts/Teacher.sol
@@ -3,7 +3,6 @@ import "./User.sol";
 
 
 contract Teacher is User {
-    function Teacher(bytes32
-      _name, bytes32 _surname) public User(_name, _surname) {
+    function Teacher(bytes32 _name, bytes32 _surname) public User(_name, _surname) {
     }
 }
