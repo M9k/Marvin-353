8127b6e4801012c46f984f70f6a15016348215f1
gmarraffa <marraffa3ai@gmail.com>
2018-03-20 15:28:10 +0100

    Components to display a list of string and offer a button to remove a string

---
+++ src/components/lists/RemoveList.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, ListGroup, ListGroupItem } from 'react-bootstrap';
+
+class RemoveList extends React.Component{
+  constructor(props){
+    super(props)
+  }
+
+  removeTrigger(e){
+    e.preventDefault();
+    this.props.removeFnc(e.target.id);
+  }
+
+  getElements(){
+    return this.props.elements.map((element, index) => {
+      return (
+        <ListGroupItem>
+          {element}
+          <Button onClick={this.removeTrigger} id={index} bsClass="deleteBtn" bsSize="small">
+            remove
+          </Button>
+        </ListGroupItem>
+      )
+    });
+  }
+
+  render(){
+    return(
+      <ListGroup>
+        {this.getElements()}
+      </ListGroup>
+    );
+  }
+}
+
+RemoveList.propTypes = {
+  elements: PropTypes.arrayOf(String),
+  removeFnc: PropTypes.func,
+};
+
+RemoveList.defaultProps = {
+  elements: [],
+  removeFnc: () => {},
+};
+
+export default RemoveList;
