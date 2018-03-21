749d02b9dd1db0c28237c1512dfe22f11b2eb2da
gmarraffa <marraffa3ai@gmail.com>
2018-03-21 13:20:50 +0100

    Test per AdminEmployerContainer

---
+++ testnpm/containers/AdminEmployerContainer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import AdminEmployerContainer, { AdminEmployerComponent } from '../../src/containers/AdminEmployerContainer';
+import { universityAction } from '../../src/actions/actions';
+import FormAddAdmin from '../../src/components/form/FormAddAdmin';
+import { shallowWithStore, createMockStore } from '../helpers/component-with-store';
+import { expect } from '../helpers/chai-enzyme';
+
+describe('<AdminEmplyoerCointainer/>', () => {
+  it('should render correctly', () => {
+    const wrapper = shallowWithStore(<AdminEmployerComponent />, createMockStore({}));
+    expect(wrapper).to.have.exactly(1).descendants('h2');
+    expect(wrapper).to.have.exactly(1).descendants(FormAddAdmin);
+    expect(wrapper.find('div').first()).to.have.id('admin-employer');
+  });
+  it('should dispatch the correct actions', () => {
+    const store = createMockStore({});
+    const wrapper = shallowWithStore(<AdminEmployerContainer />, store);
+    wrapper.props().addAdmin('pippo');
+    expect(store.isActionDispatched({
+      type: universityAction.ADD_NEW_ADMIN,
+      address: 'pippo',
+    })).to.be.true;
+    expect(store.isActionDispatched({
+      type: universityAction.ADD_NEW_ADMIN,
+      address: 'pippoooooo',
+    })).to.be.false;
+  });
+});
--- testnpm/helpers/component-with-store.js
+++ testnpm/helpers/component-with-store.js
@@ -1,27 +1,24 @@
-import { shallow, mount, render } from 'enzyme'
+import { shallow, mount, render } from 'enzyme';
 import { createMockStore } from 'redux-test-utils';
 
 require('jsdom-global')();
 
 const shallowWithStore = (component, store) => {
-  store = createMockStore(store);
   const context = {
     store,
   };
   return shallow(component, { context });
 };
 const mountWithStore = (component, store) => {
-  store = createMockStore(store);
   const context = {
     store,
   };
   return mount(component, { context });
 };
 const renderWithStore = (component, store) => {
-  store = createMockStore(store);
   const context = {
     store,
   };
   return render(component, { context });
 };
-export { shallowWithStore, mountWithStore, renderWithStore };
+export { shallowWithStore, mountWithStore, renderWithStore, createMockStore };
