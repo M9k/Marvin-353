43712a66a12517c018d3eb61a9c97592d1f6d81c
Mirco <mcailotto96@gmail.com>
2018-03-29 23:27:29 +0200

    Refractoring web3calls

--- src/sagas/userSaga.js
+++ src/sagas/userSaga.js
@@ -1,12 +1,11 @@
 import { call, put } from 'redux-saga/effects';
 import { userAction } from '../actions/actions';
-import login from '../web3calls/login';
+import { login } from '../web3calls/University';
 
 export function* tryLogin() {
-  // call the web3call for try to login
+  console.log('trying login');
   const userType = yield call(login);
-  console.log('You are a: ');
-  console.log(Number(userType));
+  console.log(`You are a ${Number(userType)}`);
   // update the user state via reducer
   yield put({ type: userAction.USER_LOGGED_IN, role: Number(userType) });
 }
--- src/web3calls/UniversityAdmin.js
+++ src/web3calls/UniversityAdmin.js
@@ -1,13 +1,46 @@
 const contract = require('truffle-contract');
 const contractUniversityJson = require('../../build/contracts/UniversityExam.json');
 
-function UniversityAdmin() {
+function getUniversity() {
   const contractUniversity = contract(contractUniversityJson);
-
   contractUniversity.setProvider(web3.currentProvider);
-
   return contractUniversity.deployed();
 }
 
-export default UniversityAdmin;
+function isAdmin() {
+  console.log('is admin');
+  const contractUniversityAdmin = getUniversity();
+  return contractUniversityAdmin.then(instance =>
+    instance.isAdmin.call());
+}
+
+function getAdminAt(adminIndex) {
+  console.log(`getting admin at ${adminIndex}`);
+  const contractUniversityAdmin = getUniversity();
+  return contractUniversityAdmin.then(instance =>
+    instance.getAdminAt.call(adminIndex)).then(adminAddress => adminAddress);
+}
+
+function getAdminNumber() {
+  console.log('getting admin number');
+  const contractUniversityAdmin = getUniversity();
+  return contractUniversityAdmin.then(instance =>
+    instance.getAdminNumber.call());
+}
+
+function addNewAdmin(addressToAdd) {
+  console.log(`adding ${addressToAdd}`);
+  const contractUniversityAdmin = getUniversity();
+  return contractUniversityAdmin.then(instance =>
+    instance.addNewAdmin(addressToAdd, { from: web3.eth.accounts[0] }));
+}
+
+function removeAdmin(addressToRemove) {
+  console.log(`removing ${addressToRemove}`);
+  const contractUniversityAdmin = getUniversity();
+  return contractUniversityAdmin.then(instance =>
+    instance.removeAdmin(addressToRemove, { from: web3.eth.accounts[0], gas: 60000 }));
+}
+
+export { isAdmin, getAdminAt, getAdminNumber, addNewAdmin, removeAdmin };
 
