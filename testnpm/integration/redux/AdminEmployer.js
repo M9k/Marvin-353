79e7fdb60a4a52176ec11a52377573d368fd7be6
gmarraffa <marraffa3ai@gmail.com>
2018-04-13 13:46:50 +0200

    Lint

--- testnpm/integration/redux/AdminEmployer.js
+++ testnpm/integration/redux/AdminEmployer.js
@@ -1,79 +1,69 @@
 import { expectSaga } from 'redux-saga-test-plan';
 import * as matchers from 'redux-saga-test-plan/matchers';
 import { throwError } from 'redux-saga-test-plan/providers';
-import reducer, { creators } from '../../../src/ducks/AdminEmployer';
+import reducer from '../../../src/ducks/AdminEmployer';
 import * as sagas from '../../../src/sagas/AdminEmployerSaga';
 import * as UniversityAdmin from '../../../src/web3calls/UniversityAdmin';
 
 const failedState = {
   loading: false,
   errored: true,
-  adminAccount: []
+  adminAccount: [],
 };
 const filledState = {
   errored: false,
   loading: false,
-  adminAccount: ["topolino", "pippo", "paperino"]
-}
+  adminAccount: ['topolino', 'pippo', 'paperino'],
+};
 
 describe('add new admin feature', () => {
-  it("should succesfully add the admin provided", () => {
-    return expectSaga(sagas.addAdmin, { address: "pippo" })
-      .withReducer(reducer)
-      .provide([
-        [matchers.call.fn(UniversityAdmin.addNewAdmin), true]
-      ])
-      .hasFinalState({
-        loading: false,
-        errored: false,
-        adminAccount: ["pippo"]
-      })
-      .run();
-  });
-  it("should fail gently when add an admin that already exist", () => {
-    return expectSaga(sagas.addAdmin, { address: "pippo" })
+  it('should succesfully add the admin provided', () => expectSaga(sagas.addAdmin, { address: 'pippo' })
+    .withReducer(reducer)
+    .provide([
+      [matchers.call.fn(UniversityAdmin.addNewAdmin), true],
+    ])
+    .hasFinalState({
+      loading: false,
+      errored: false,
+      adminAccount: ['pippo'],
+    })
+    .run());
+  it('should fail gently when add an admin that already exist', () => expectSaga(sagas.addAdmin, { address: 'pippo' })
     .withReducer(reducer, filledState)
     .provide([
-      [matchers.call.fn(UniversityAdmin.addNewAdmin), throwError(new Error())]
+      [matchers.call.fn(UniversityAdmin.addNewAdmin), throwError(new Error())],
     ])
     .hasFinalState(Object.assign({}, filledState, { errored: true }))
-    .run()
-  });
+    .run());
 });
-describe('remove admin feature', ()=> {
-  it("should remove the correct admin", () => {
-    return expectSaga(sagas.removeAdmin, { address: "pippo"} )
-      .withReducer(reducer, filledState)
-      .provide([
-        [matchers.call.fn(UniversityAdmin.removeAdmin), true]
-      ])
-      .hasFinalState({
-        loading: false,
-        errored: false,
-        adminAccount: ["topolino", "paperino" ]
-      })
-      .run();
-  });
-  it("shouldnt remove an admin that not exist", () => {
-    return expectSaga(sagas.removeAdmin, { address: 'pluto' })
+describe('remove admin feature', () => {
+  it('should remove the correct admin', () => expectSaga(sagas.removeAdmin, { address: 'pippo' })
     .withReducer(reducer, filledState)
     .provide([
-      [matchers.call.fn(UniversityAdmin.removeAdmin), true]
+      [matchers.call.fn(UniversityAdmin.removeAdmin), true],
+    ])
+    .hasFinalState({
+      loading: false,
+      errored: false,
+      adminAccount: ['topolino', 'paperino'],
+    })
+    .run());
+  it('shouldnt remove an admin that not exist', () => expectSaga(sagas.removeAdmin, { address: 'pluto' })
+    .withReducer(reducer, filledState)
+    .provide([
+      [matchers.call.fn(UniversityAdmin.removeAdmin), true],
     ])
     .hasFinalState(filledState)
-    .run();
-  })
+    .run());
 });
-describe("when web3 is down", () => {
-  [sagas.addAdmin, sagas.removeAdmin, sagas.getAllAdmins].forEach(feature => {
-    it(`all features should fail gently`, () => {
-      return expectSaga(feature)
+describe('when web3 is down', () => {
+  [sagas.addAdmin, sagas.removeAdmin, sagas.getAllAdmins].forEach((feature) => {
+    it('all features should fail gently', () => expectSaga(feature)
       .withReducer(reducer)
       .provide([
-        [matchers.call.fn(UniversityAdmin.addNewAdmin), throwError(new Error())]
+        [matchers.call.fn(UniversityAdmin.addNewAdmin), throwError(new Error())],
       ])
       .hasFinalState(failedState)
-      .run()
-    });
-  })
+      .run());
+  });
 });
