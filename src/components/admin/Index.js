6b9f41c962955d1c5b2e9ec99d5825f691d2fe1f
Parwinder Singh <33651643+SinghParwinder@users.noreply.github.com>
2018-04-15 15:38:23 +0200

    Factory e resto degli utenti

---
+++ src/components/admin/Index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Index = () => (
+  <div>
+    <h1 className="text-center">Work in progress!</h1>
+    <img alt="" className="img-responsive imageIndexTop" src="./media/bulding.png" />
+  </div>
+);
+export default Index;
