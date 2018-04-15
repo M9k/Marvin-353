948a6aa26c1b6f7e514452c725f7e1d30bf85b66
Parwinder Singh <33651643+SinghParwinder@users.noreply.github.com>
2018-04-15 17:03:36 +0200

    Pagina teacher with all cards

--- src/components/admin/Index.js
+++ src/components/admin/Index.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import CardWithIcon from '../custom/CardWithIcon';
 
 const Index = () => (
   <div>
-    <h1 className="text-center">Work in progress!</h1>
-    <img alt="" className="img-responsive imageIndexTop" src="./media/bulding.png" />
+    <h3 className="text-center">Welcome Admin</h3>
+    <CardWithIcon
+      title="Confirm teacher accounts"
+      text="Confirm pending unconfirmed users for teacher role."
+      image="default.png"
+      links={[{ path: '/', label: 'Click here to confirm' }]}
+    />
+    <CardWithIcon
+      title="Confirm student accounts"
+      text="Confirm pending unconfirmed users for student role."
+      image="default.png"
+      links={[{ path: '/', label: 'Click here to confirm' }]}
+    />
+    <CardWithIcon
+      title="Manage teacher accounts"
+      text="Manage all teacher accounts present in the University"
+      image="default.png"
+      links={[{ path: '/', label: 'Click here to manage' }]}
+    />
+    <CardWithIcon
+      title="Manage student accounts"
+      text="Manage all student accounts present in the University"
+      image="default.png"
+      links={[{ path: '/', label: 'Click here to manage' }]}
+    />
   </div>
 );
 export default Index;
