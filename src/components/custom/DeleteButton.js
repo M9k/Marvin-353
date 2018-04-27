f2a3d5c9ead844ce536d38a24c16ee209047f0f6
Parwinder Singh <33651643+SinghParwinder@users.noreply.github.com>
2018-04-27 17:15:59 +0200

    Model Delete Button

--- src/components/custom/DeleteButton.js
+++ src/components/custom/DeleteButton.js
@@ -1,21 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button } from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
 
 
 class deleteButton extends React.Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
+    this.handle = this.handle.bind(this);
+    this.close = this.close.bind(this);
+    this.state = { showing: false };
   }
-
-  handleClick() {
+  handle() {
     this.props.deleteFunction(this.props.objectToRemove.item);
+    this.setState({
+      showing: false,
+    });
   }
-
+  handleClick() {
+    this.setState({
+      showing: true,
+    });
+  }
+  close() {
+    this.setState({
+      showing: false,
+    });
+  }
   render() {
     return (
-      <Button bsStyle="danger" onClick={this.handleClick}>Delete</Button>
+      <div>
+        <Button bsStyle="danger" onClick={this.handleClick}>Delete</Button>
+        <Modal show={this.state.showing}>
+          <Modal.Header>
+            Delete confirmation
+          </Modal.Header>
+          <Modal.Body>
+            Are you sure you want to delete {this.props.objectToRemove.item}?
+          </Modal.Body>
+          <Modal.Footer>
+            <Button bsStyle="danger" onClick={this.handle}>Yes</Button>
+            <Button bsStyle="info" onClick={this.close}>No</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
     );
   }
 }
