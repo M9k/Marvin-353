89281eef58dc4df2e45484f82ec874de2fa755b1
gmarraffa <marraffa3ai@gmail.com>
2018-04-27 12:34:09 +0200

    Test suite

--- src/ducks/TeacherExam.js
+++ src/ducks/TeacherExam.js
@@ -1,22 +1,40 @@
 import Duck from 'extensible-duck';
+
 const TeacherExamDuck = new Duck({
   namespace: 'marvin',
   store: 'TeacherExam',
-  types: [],
+  types: ['PUSH_EXAM', 'LIST_LOADING', 'LIST_ERRORED', 'LIST_FINISHED'],
   initialState: {},
   reducer: (state, action, duck) => {
     const { types } = duck;
     const { initialState } = duck;
-    switch(action.type){
-
+    switch (action.type) {
+      case (types.PUSH_EXAM):
+      case (types.LIST_LOADING):
+      case (types.LIST_ERRORED):
+      case (types.LIST_FINISHED):
+      default:
+        return state;
     }
   },
   selectors: {
-
+    examByCode: state => ({}),
+    examByCourse: state => ({}),
   },
-  creators: duck => ({
-
-  })
+  creators: ({ types }) => ({
+    pushExam: exam => (
+      { type: types.PUSH_EXAM, exam }
+    ),
+    listIsLoading: () => (
+      { type: types.LIST_LOADING }
+    ),
+    listHasErrored: () => (
+      { type: types.LIST_ERRORED }
+    ),
+    listHasFinished: () => (
+      { type: types.LIST_FINISHED }
+    ),
+  }),
 });
 export const { creators, selectors, initialState } = TeacherExamDuck;
 export default TeacherExamDuck.reducer;
--- src/sagas/TeacherExamSaga.js
+++ src/sagas/TeacherExamSaga.js
@@ -1,10 +1,24 @@
-import { call, put, fork } from 'redux-saga/effects';
+import { call, put, fork, takeLatest } from 'redux-saga/effects';
 import { creators as actionCreators } from '../ducks/TeacherExam';
 
 const actionType = type => `marvin/TeacherExamSaga/${type}`;
+const GET_LIST = actionType('GET_LIST');
 
-export const creators = {};
+export function* getExamData(userAddress, examIndex){
+
+}
+
+export function* getList({ userAddress }){
+
+}
+export const creators = {
+  getListAction: userAddress => (
+    { type: GET_LIST, userAddress }
+  ),
+};
 
 export default function* handler() {
-  yield [];
-};
+  yield [
+    fork(takeLatest, GET_LIST, getList),
+  ];
+}
