ce775dbebf75bc07c60d50ce099d60be0670b2e7
Elena Mattiazzo <ele.mattiazzo@gmail.com>
2018-04-28 10:38:23 +0200

    Aggiunto filtro corsi per anno solare

--- src/components/admin/AdminCourses.js
+++ src/components/admin/AdminCourses.js
@@ -1,32 +1,19 @@
 import React from 'react';
+import { FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
 import PageTableForm from '../template/PageTableForm';
 import Form from '../custom/Form';
 import Utils from '../custom/utils';
 import FieldTypes from '../custom/fieldtypes';
-import Field from '../custom/Field';
 
 class AdminCourses extends React.Component {
-  static changeYear() {
-    console.log('CHANGEEE');
-    /* da finire
-    const select = document.getElementsByClassName('form-control');
-    const year = select[2].getState();
-    console.log(year);
-    // const year = select.options[select.selectedIndex].value;
-    console.log(select);
-    // console.log(year);
-    /*
-    this.list = this.degreeCoursesList.filter(course => course.solarYear === year);
-    */
-  }
   constructor(props) {
     super(props);
     this.solarYears = [
       '2017',
       '2016',
+      '2015',
     ];
-    this.getCourses = () => {
-    };
+    this.getCourses = () => {};
     this.degreeCoursesList = [
       {
         code: 'L-31',
@@ -40,8 +27,26 @@ class AdminCourses extends React.Component {
         code: 'L-35',
         solarYear: '2017',
       },
+      {
+        code: 'L-30',
+        solarYear: '2015',
+      },
     ];
     this.list = this.degreeCoursesList;
+    this.state = { year: 'ALL' };
+    this.onChangeYear = this.onChangeYear.bind(this);
+  }
+  onChangeYear() {
+    if (this.state.year !== this.inputEl.value) {
+      this.setState({ year: this.inputEl.value });
+      this.changeTable();
+    }
+  }
+  changeTable() {
+    this.list = this.degreeCoursesList;
+    if (this.inputEl.value !== 'ALL') {
+      this.list = this.degreeCoursesList.filter(course => course.solarYear === this.inputEl.value);
+    }
   }
   render() {
     /*
@@ -52,6 +57,12 @@ class AdminCourses extends React.Component {
         </Link>;
     }
     */
+    const options = [];
+    options.push(<option value="ALL">ALL</option>);
+    for (let i = 0; i < this.solarYears.length; i += 1) {
+      options.push(<option value={this.solarYears[i]}>{this.solarYears[i]}</option>);
+    }
+
     return (
       <div>
         <Form
@@ -76,14 +87,17 @@ class AdminCourses extends React.Component {
           ]}
           submitFunction={null}
         />
-        <Field
-          name="filterYear"
-          label="Filter course by academic Year"
-          type={FieldTypes.SELECT}
-          values={this.solarYears}
-          validateFunction={Utils.notNullValue}
-          onChangeValue={this.changeYear}
-        />
+        <FormGroup controlId="selectYear">
+          <ControlLabel>Filter by academic year</ControlLabel>
+          <FormControl
+            onChange={this.onChangeYear}
+            inputRef={el => this.inputEl = el} // eslint-disable-line no-return-assign
+            componentClass="select"
+            placeholder="select"
+          >
+            {options}
+          </FormControl>
+        </FormGroup>
         <PageTableForm
           getTableData={this.getCourses}
           tableData={this.list}
