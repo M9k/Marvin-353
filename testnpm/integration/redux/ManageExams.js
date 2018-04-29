c0211fca0c9fed59c6f769b12c51599bbf6a26ac
gmarraffa <marraffa3ai@gmail.com>
2018-04-29 18:29:29 +0200

    Set up integration test

--- testnpm/integration/redux/ManageExams.js
+++ testnpm/integration/redux/ManageExams.js
@@ -1,13 +1,12 @@
 import { expectSaga } from 'redux-saga-test-plan';
 import * as matchers from 'redux-saga-test-plan/matchers';
 import { throwError } from 'redux-saga-test-plan/providers';
-import reducer, { initialState, creators } from '../../../src/ducks/ManageExams';
+import CourseReducer, { creators as CourseCreators } from '../../../src/ducks/CourseExams';
+import TeacherReducer, { creators as TeacherCreators } from '../../../src/ducks/TeachersList';
+import ExamsReducer, { creators as ExamsCreators } from '../../../src/ducks/ExamsList';
 import * as sagas from '../../../src/sagas/ManageExamsSaga';
 import GentlyFail from './GentlyFail';
 
-const features = [];
-const failedState = {};
 
 describe('ManageExams feature', () => {
-  GentlyFail(features, reducer, failedState);
 });
