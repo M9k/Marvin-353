7261c746e22ff4090cb8ee511a8fb916d9c1c493
Parwinder Singh <33651643+SinghParwinder@users.noreply.github.com>
2018-04-30 23:06:57 +0200

    aggiornato confirmStudent

--- src/components/admin/ConfirmStudentUser.js
+++ src/components/admin/ConfirmStudentUser.js
@@ -1,32 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import Form from '../custom/Form';
-import FieldTypes from '../custom/fieldtypes';
-import Utils from '../custom/utils';
 import PageTableForm from '../template/PageTableForm';
 import { creators } from '../../sagas/AdminSaga';
+import ROLES from '../../util/logic/AccountEnum';
 
 
 const ConfirmStudent = props => (
   <div>
-    <Form
-      description="Confirm student account"
-      fields={[{
-        name: 'studentAddress',
-        label: 'Address:',
-        help: 'insert the address of the student',
-        placeholder: '0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf',
-        type: FieldTypes.TEXT,
-        validateFunction: Utils.validEthAddress,
-      }]}
-      submitFunction={props.confirmStudent}
-    />
     <PageTableForm
       getTableData={props.getPendingStudents}
       tableData={props.studentAccounts}
-      deleteMultiColumnRow={props.denyStudent}
-      headerInfo={['name', 'surname', 'address', 'course', 'deny']}
+      deleteSingleColumnRow={props.denyStudent}
+      confirmationFunction={props.confirmStudent}
+      headerInfo={['Address', 'Confirm', 'Deny']}
     />
   </div>
 );
@@ -45,10 +32,8 @@ const mapStateToProps = state => ({
 
 function mapDispatchToProps(dispatch) {
   return {
-    confirmStudent: objArr => (
-      dispatch(creators.approveUserAction(objArr.addressStudent.value))
-    ),
-    denyStudent: address => dispatch(creators.removeUserAction(address)),
+    confirmStudent: add => dispatch(creators.approveUserAction(ROLES.UNCONFIRMED_STUDENT, add)),
+    denyStudent: add => dispatch(creators.removeUserAction(ROLES.UNCONFIRMED_STUDENT, add)),
     getPendingStudents: () => dispatch(creators.getPendingStudentsAction())
     ,
   };
