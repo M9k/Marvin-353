aed00e88bdff73d4dac65f58ff6a9cf34eb5b095
Elena Mattiazzo <ele.mattiazzo@gmail.com>
2018-04-30 19:34:17 +0200

    Corretto test

--- testnpm/components/custom/DetailsButton.js
+++ testnpm/components/custom/DetailsButton.js
@@ -18,18 +18,6 @@ const exam = {
   courseAddress: '0x4bd1280852cadb002734647305afc1db7ddd6acb',
   professorAddress: '0x79196b90d1e952c5a43d4847caa08d50b967c34a',
 };
-const exam2 = {
-  name: 'Logic',
-  credits: '10',
-  courseName: 'L-16',
-  year: '2017',
-  professorName: '',
-  professorSurname: '',
-  examAddress: '0x811da72aca31e56f770fc33df0e45fd08720e157',
-  mandatory: true,
-  courseAddress: '0x4bd1280852cadb002734647305afc1db7ddd6acb',
-  professorAddress: '',
-};
 describe('DetailsButton component', () => {
   it('Should render the component', () => {
     const wrapper = shallow(<DetailsButton object={exam} text="Details" />);
