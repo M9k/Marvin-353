c566b8a5b3b2d301ce95b73f71531d6d8771d402
gmarraffa <marraffa3ai@gmail.com>
2018-04-30 11:38:57 +0200

    getData sagas for ManageExams

--- src/sagas/ManageExamsSaga.js
+++ src/sagas/ManageExamsSaga.js
@@ -1,13 +1,59 @@
-import { call, put, fork } from 'redux-saga/effects';
+import { call, put, fork, all } from 'redux-saga/effects';
+import * as User from '../web3calls/User';
+import * as Exam from '../web3calls/Exam';
 
 const actionType = type => `marvin/ManageExamsSaga/${type}`;
+const ADD_NEW_EXAM = actionType('ADD_NEW_EXAM');
+const GET_ALL_EXAMS = actionType('GET_ALL_EXAMS');
+const GET_EXAMS_BY_COURSE = actionType('GET_EXAM_BY_COURSE');
+const GET_TEACHERS = actionType('GET_TEACHERS');
+const APTCE = actionType('ASSOCIATE_PROFESSOR_TO_COURSE_EXAM');
+const APTE = actionType('ASSOCIATE_PROFESSOR_TO_EXAM');
 
-function* getExamData(examAddress) {
-  yield call();
-  return {};
+export function* getTeacherData(teacherAddress) {
+  const dataFetch = [
+    call(User.getName, teacherAddress),
+    call(User.getSurname, teacherAddress),
+  ];
+  const [name, surname] = yield all(dataFetch);
+  return {
+    address: teacherAddress,
+    name,
+    surname,
+  }
 }
-export const creators = {};
+export function* getExamData(examAddress) {
+  const address = examAddress;
+  const dataFetch = [
+    call(Exam.getName, address),
+    call(Exam.getCredits, address),
+    call(Exam.getObligatoriness, address),
+    call(Exam.getTeacherContract, address),
+  ];
+  const [name, credits, mandatory, teacherAddress] = yield all(dataFetch);
+  const teacherData = yield call(getTeacherData, teacherAddress);
+  return {
+    address,
+    name,
+    credits,
+    mandatory,
+    teacherAddress,
+    teacherName: teacherData.name,
+    teacherSurname: teacherData.surname,
+  };
+}
+export const creators = {
+  addNewExamAction: (courseAddress, name, credits, mandatory) => (
+    {
+      type: ADD_NEW_EXAM,
+      courseAddress,
+      name,
+      credits,
+      mandatory,
+    }
+  ),
+};
 
 export default function* handler() {
   yield [];
-};
+}
---
+++ testnpm/sagas/ManageExamsSaga.js
@@ -0,0 +1,66 @@
+import { put, call } from 'redux-saga/effects';
+import { expectSaga } from 'redux-saga-test-plan';
+import { throwError } from 'redux-saga-test-plan/providers';
+import * as matchers from 'redux-saga-test-plan/matchers';
+import * as sagas from '../../src/sagas/ManageExamsSaga';
+import * as Exam from '../../src/web3calls/Exam';
+import * as User from '../../src/web3calls/User';
+
+describe('ManageExamsSaga', () => {
+  function* sagaStub(saga, address) {
+    try {
+      const data = yield call(saga, address);
+      yield put(data);
+    } catch (e) {
+      yield put('error');
+    }
+  }
+  describe('getExamData', () => {
+    it('should retrieve the correct data', () => expectSaga(sagaStub, sagas.getExamData, '0')
+      .provide([
+        [matchers.call.fn(Exam.getName, '0'), 'Analisi Matematica'],
+        [matchers.call.fn(Exam.getCredits, '0'), 12],
+        [matchers.call.fn(Exam.getObligatoriness, '0'), true],
+        [matchers.call.fn(Exam.getTeacherContract, '0'), 'sart'],
+        [matchers.call.fn(sagas.getTeacherData, 'sart'), {
+          name: 'Caterina',
+          surname: 'Sartori',
+        }],
+      ])
+      .put({
+        address: '0',
+        name: 'Analisi Matematica',
+        credits: 12,
+        mandatory: true,
+        teacherAddress: 'sart',
+        teacherName: 'Caterina',
+        teacherSurname: 'Sartori',
+      })
+      .run());
+    it('should not catch the error if something goes wrong', () => expectSaga(sagaStub, sagas.getExamData, '0')
+      .provide([
+        [matchers.call.fn(Exam.getName, '0'), throwError(new Error())],
+      ])
+      .put('error')
+      .run());
+  });
+  describe('getTeacherData', () => {
+    it('should retrieve the correct data', () => expectSaga(sagaStub, sagas.getTeacherData, 'sart')
+      .provide([
+        [matchers.call.fn(User.getName, 'sart'), 'Caterina'],
+        [matchers.call.fn(User.getSurname, 'sart'), 'Sartori'],
+      ])
+      .put({
+        address: 'sart',
+        name: 'Caterina',
+        surname: 'Sartori',
+      })
+      .run());
+    it('should not catch the error if something goes wrong', () => expectSaga(sagaStub, sagas.getTeacherData, 'sart')
+      .provide([
+        [matchers.call.fn(User.getName, 'sart'), throwError(new Error())],
+      ])
+      .put('error')
+      .run());
+  });
+});
