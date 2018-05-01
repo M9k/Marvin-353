56491c5d51c969ee67d136bc65e62bf29551eff0
gmarraffa <marraffa3ai@gmail.com>
2018-05-01 18:19:12 +0200

    Aggiunto adapter su data getter

--- src/sagas/helpers/getters.js
+++ src/sagas/helpers/getters.js
@@ -3,19 +3,19 @@ import * as User from '../../web3calls/User';
 import * as Exam from '../../web3calls/Exam';
 import * as Course from '../../web3calls/Course';
 
-export function* getTeacherData(teacherAddress) {
+export function* getTeacherData(teacherAddress, adapter = obj => obj) {
   const dataFetch = [
     call(User.getName, teacherAddress),
     call(User.getSurname, teacherAddress),
   ];
   const [name, surname] = yield all(dataFetch);
-  return {
+  return adapter({
     address: teacherAddress,
     name,
     surname,
-  };
+  });
 }
-export function* getExamData(examAddress) {
+export function* getExamData(examAddress, adapter = obj => obj) {
   const address = examAddress;
   const dataFetch = [
     call(Exam.getName, address),
@@ -30,7 +30,7 @@ export function* getExamData(examAddress) {
   } else {
     teacherData = yield call(getTeacherData, teacherAddress);
   }
-  return {
+  return adapter({
     address,
     name,
     credits,
@@ -38,21 +38,21 @@ export function* getExamData(examAddress) {
     teacherAddress,
     teacherName: teacherData.name,
     teacherSurname: teacherData.surname,
-  };
+  });
 }
-export function* getCourseData(courseAddress) {
+export function* getCourseData(courseAddress, adapter = obj => obj) {
   const dataFetch = [
     call(Course.getName, courseAddress),
     call(Course.getSolarYear, courseAddress),
   ];
   const [courseName, solarYear] = yield all(dataFetch);
-  return {
+  return adapter({
     courseName,
     courseAddress,
     solarYear: Number(solarYear),
-  };
+  });
 }
-export function* getCourseExamsList(courseAddress) {
+export function* getCourseExamsList(courseAddress, adapter = obj => obj) {
   const courseNumber = yield call(Course.getExamNumber, courseAddress);
   const examsAddressFetch = Array(Number(courseNumber)).fill().map((_, id) => (
     call(Course.getExamContractAt, courseAddress, id)
@@ -62,5 +62,5 @@ export function* getCourseExamsList(courseAddress) {
     call(getExamData, addr)
   ));
   const examsData = yield all(examDataFetch);
-  return examsData;
+  return examsData.map(adapter);
 }
