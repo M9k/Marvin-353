1371e2d7c5947531ab3d487900aa5ae0394681ce
Parwinder Singh <33651643+SinghParwinder@users.noreply.github.com>
2018-05-02 19:57:53 +0200

    fix build

--- testnpm/integration/redux/Student.js
+++ testnpm/integration/redux/Student.js
@@ -1,8 +1,9 @@
-import { expectSaga } from 'redux-saga-test-plan';
-import * as matchers from 'redux-saga-test-plan/matchers';
-import { throwError } from 'redux-saga-test-plan/providers';
-import reducer, { initialState, creators } from '../../../src/ducks/Student';
-import * as sagas from '../../../src/sagas/StudentSaga';
+// import { expectSaga } from 'redux-saga-test-plan';
+// import * as matchers from 'redux-saga-test-plan/matchers';
+// import { throwError } from 'redux-saga-test-plan/providers';
+// import reducer, { initialState, creators } from '../../../src/ducks/Student';
+import reducer from '../../../src/ducks/Student';
+// import * as sagas from '../../../src/sagas/StudentSaga';
 import GentlyFail from './GentlyFail';
 
 const features = [];
