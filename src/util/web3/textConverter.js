18263383bb3cc690563f0c6a2510c6b817572f2a
Mirco <mcailotto96@gmail.com>
2018-05-03 12:35:49 +0200

    Corretta conversione a testo ed 0000

--- src/util/web3/textConverter.js
+++ src/util/web3/textConverter.js
@@ -1,6 +1,6 @@
-const toBytes32 = text =>
-  (text !== null ? web3.fromAscii(text).replace(/\u0000/g, '').replace(/\0/g, '') : null);
+const toBytes32 = text => (text !== null ? web3.fromAscii(text) : null);
 
-const toText = hex => web3.toAscii(hex);
+const toText = hex =>
+  (hex !== null ? web3.toAscii(hex).replace(/\u0000/g, '').replace(/\0/g, '') : null);
 
 export { toBytes32, toText };
