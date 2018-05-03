c185c1afef585a716b159a6b61d8d09971f896b8
Elena Mattiazzo <ele.mattiazzo@gmail.com>
2018-05-03 13:59:24 +0200

    Eliminate cose inutili su AdminCourseExams

--- src/components/admin/AdminCourseExams.js
+++ src/components/admin/AdminCourseExams.js
@@ -9,7 +9,6 @@ import Utils from '../custom/utils';
 import Form from '../custom/Form';
 import PageTableForm from '../template/PageTableForm';
 import ExamDetails from './ExamDetails';
-// import { creators as courseSagaAction } from '../../sagas/CourseSaga';
 
 class AdminCourseExams extends React.Component {
   constructor(props) {
@@ -17,7 +16,6 @@ class AdminCourseExams extends React.Component {
     this.state = { showDetails: false };
     this.params = this.props.params;
     this.courseAdress = this.params.examid;
-    this.getCourses = () => {};
     this.viewDetails = this.viewDetails.bind(this);
     this.addExamBuilder = this.addExamBuilder.bind(this);
     this.getExamsByAddress = this.getExamsByAddress.bind(this);
@@ -27,42 +25,6 @@ class AdminCourseExams extends React.Component {
       courseAddress: '0xfae394561e33e242c551d15d4625309ea4c0b97f',
       totalCredits: 180,
     };
-    /*
-    this.examList = [
-      {
-        name: 'Programming',
-        credits: '10',
-        mandatory: true,
-        professorName: 'Filè',
-        professorSurname: 'Gilberto',
-        professorAddress: '0x252dae0a4b9d9b80f504f6418acd2d364c0c59cd',
-      },
-      {
-        name: 'Computer architecture',
-        credits: '8',
-        mandatory: true,
-        professorName: 'Sperduti',
-        professorSurname: 'Alessandro',
-        professorAddress: '0x252dae0a4b9d9b80f504f6418acd2d364c0c59cd',
-      },
-      {
-        name: 'Logic',
-        credits: '6',
-        mandatory: false,
-        professorName: 'Maietti',
-        professorSurname: 'Maria Emilia',
-        professorAddress: '0x252dae0a4b9d9b80f504f6418acd2d364c0c59cd',
-      },
-      {
-        name: 'Database',
-        credits: '9',
-        mandatory: true,
-        professorName: '',
-        professorSurname: '',
-        professorAddress: '0x252dae0a4b9d9b80f504f6418acd2d364c0c59cd',
-      },
-    ];
-    */
   }
   getExamsByAddress() {
     this.props.getExams(this.courseAdress);
@@ -89,16 +51,6 @@ class AdminCourseExams extends React.Component {
     return (
       <div>
         <h3 className="text-center">Course {this.params.examid}</h3>
-        <dl>
-          <dt>Name</dt>
-          <dd>{this.course.name}</dd>
-          <dt>Solar year</dt>
-          <dd>{this.course.solarYear}</dd>
-          <dt>Total credits</dt>
-          <dd>{this.course.totalCredits}</dd>
-          <dt>Address</dt>
-          <dd>{this.course.courseAddress}</dd>
-        </dl>
         <Form
           description="Add a new exam"
           fields={[
