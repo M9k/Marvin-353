ce536f342515976e0b1e04bc7da9cbe4380777c2
Parwinder Singh <33651643+SinghParwinder@users.noreply.github.com>
2018-05-04 17:32:04 +0200

    Index student da sistemare StudentSaga

--- src/components/student/Index.js
+++ src/components/student/Index.js
@@ -1,41 +1,71 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import ProgressBar from 'react-bootstrap/lib/ProgressBar';
 import Jumbotron from 'react-bootstrap/lib/Jumbotron';
 import CardWithIcon from '../custom/CardWithIcon';
+import { creators } from '../../sagas/StudentSaga';
 
-
-const Index = () => (
-  <div>
-    <div>
-      <Jumbotron>
-        <h1>Welcome Student</h1>
-        <p>
-          Here you can enrol to your exams,
+class Index extends React.Component {
+  componentWillMount() {
+    this.props.getCredits(this.props.myAddress);
+  }
+  render() {
+    return (
+      <div>
+        <Jumbotron>
+          <h1>Welcome Student</h1>
+          <p>
+              Here you can enrol to your exams,
 check how many credits you need to end and you can choose and add optional exams to your study plan.
-        </p>
-        <p>
-          <h5>Your total credits: {120}</h5>
-          <h5>Total credits need to end: {180}</h5>
-          <ProgressBar active bsStyle="success" now={(120 / 180) * 100} />
-        </p>
-      </Jumbotron>
+          </p>
+          <h5>Your total credits: {this.props.creditsObj.credits}</h5>
+          <h5>Total credits need to end: {this.props.creditsObj.graduationCredits}</h5>
+          <ProgressBar
+            active
+            bsStyle="success"
+            now={(this.props.creditsObj.credits / this.props.creditsObj.graduationCredits) * 100}
+          />
+        </Jumbotron>
+        <CardWithIcon
+          title="Exams list"
+          text="Show all my exams list"
+          image="examsList.png"
+          links={[{ path: '/exams', label: 'Click here to see your exams' }]}
+        />
+        <CardWithIcon
+          title="Optional exams"
+          text="Show all optional exams for my course"
+          image="examsList.png"
+          links={[{ path: '/optionalexams', label: 'Click here to see optional exams' }]}
+        />
+      </div>
+    );
+  }
+}
+Index.propTypes = {
+  creditsObj: PropTypes.shape({
+    credits: PropTypes.number,
+    graduationCredits: PropTypes.number,
+  }),
+  getCredits: PropTypes.func.isRequired,
+  myAddress: PropTypes.string.isRequired,
+};
 
-    </div>
-    <div>
-      <CardWithIcon
-        title="Exams list"
-        text="Show all my exams list"
-        image="examsList.png"
-        links={[{ path: '/exams', label: 'Click here to see your exams' }]}
-      />
-      <CardWithIcon
-        title="Optional exams"
-        text="Show all optional exams for my course"
-        image="examsList.png"
-        links={[{ path: '/optionalexams', label: 'Click here to see optional exams' }]}
-      />
-    </div>
-  </div>
-);
+Index.defaultProps = {
+  creditsObj: { credits: 0, graduationCredits: 0 },
+};
 
-export default Index;
+const mapStateToProps = state => ({
+  credits: state.student.credits,
+  myAddress: state.user.data.contract,
+});
+
+function mapDispatchToProps(dispatch) {
+  return {
+    getCredits: add =>
+      dispatch(creators.getCreditsAction(add)),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Index);
