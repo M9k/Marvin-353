249c96a7e525d61d2daafa4ce11534bfb452dd1f
Elena Mattiazzo <ele.mattiazzo@gmail.com>
2018-05-05 18:11:03 +0200

    Tolto commento test

--- testnpm/components/routes/AdminRoutes.js
+++ testnpm/components/routes/AdminRoutes.js
@@ -1,10 +1,10 @@
-/* import assert from 'assert';
+import assert from 'assert';
 import AdminRoutes from '../../../src/components/routes/AdminRoutes';
 
 describe('AdminRoutes', () => {
   it('should return an array with three elements', () => {
     const routes = Object.keys(AdminRoutes).length;
-    assert.equal(routes, 6);
+    assert.equal(routes, 7);
   });
 });
-*/
+
