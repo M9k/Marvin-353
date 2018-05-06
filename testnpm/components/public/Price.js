c666490eac105df18daf4c8196c1f0595de79507
Elena Mattiazzo <ele.mattiazzo@gmail.com>
2018-05-06 23:17:10 +0200

    Corretti import

--- testnpm/components/public/Price.js
+++ testnpm/components/public/Price.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme'; // eslint-disable-line import/no-extraneous-dependencies
 import assert from 'assert';
-import { Image } from 'react-bootstrap';
-import { expect } from 'chai'; // eslint-disable-line import/no-extraneous-dependencies
 import Price from '../../../src/components/public/Price';
 
 describe('Price component', () => {
