655c3a7c9d565edc644ff96903b8d2d09c1eef50
Elena Mattiazzo <ele.mattiazzo@gmail.com>
2018-05-06 16:26:44 +0200

    Tolte cose inutili da CardWithIcon e migliorato test

--- testnpm/components/custom/CardWithIcon.js
+++ testnpm/components/custom/CardWithIcon.js
@@ -4,24 +4,50 @@ import { shallow, mount } from 'enzyme';
 import assert from 'assert';
 import CardWithIcon from '../../../src/components/custom/CardWithIcon';
 // eslint-disable-next-line
-import {expect} from "chai";
+import { expect } from "chai";
 
 describe('CardWithIcon component', () => {
-  it('should render the component', () => {
+  it('Should render the component with more that one link', () => {
+    const links = [
+      {
+        path: '/courses',
+        label: 'Courses',
+      },
+      {
+        path: '/exams',
+        label: 'Exams',
+      },
+    ];
+    const wrapper = shallow(<CardWithIcon title="Test" text="Test" links={links} />);
+    assert.equal(wrapper.length, 1);
+    assert.equal(wrapper.html().search('<a') !== -1, true);
+  });
+  it('Should render the component with only one link', () => {
+    const links = [
+      {
+        path: '/courses',
+        label: 'Courses',
+      },
+    ];
+    const wrapper = shallow(<CardWithIcon title="Test" text="Test" links={links} />);
+    assert.equal(wrapper.length, 1);
+    assert.equal(wrapper.html().search('<a') !== -1, true);
+  });
+  it('Should render the component', () => {
     const wrapper = shallow(<CardWithIcon title="Test" text="Test" />);
     assert.equal(wrapper.length, 1);
   });
-  it('should have a title', () => {
+  it('Should have a title', () => {
     const props = { title: 'Test title', text: 'Test text' };
     const wrapper = shallow(<CardWithIcon {...props} />);
     assert.equal(wrapper.html().search('<h5') !== -1, true);
   });
-  it('should have a text', () => {
+  it('Should have a text', () => {
     const props = { title: 'Test title', text: 'Test text' };
     const wrapper = shallow(<CardWithIcon {...props} />);
     assert.equal(wrapper.html().search('<p') !== -1, true);
   });
-  it('should save correct props', () => {
+  it('Should save correct props', () => {
     const props = { title: 'Test title', text: 'Test text' };
     const wrapper = mount(<CardWithIcon {...props} />);
     expect(wrapper.props().title).to.equal('Test title');
