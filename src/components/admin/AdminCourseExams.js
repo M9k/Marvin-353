8dfbd6dd0b57033536c086d8770f48395a1b67ed
Mirco <mcailotto96@gmail.com>
2018-05-07 02:48:52 +0200

    Non accettare valore vuoto

--- src/components/admin/AdminCourseExams.js
+++ src/components/admin/AdminCourseExams.js
@@ -85,7 +85,7 @@ export class AdminCourseExams extends React.Component {
               help: 'is the exam optional',
               type: FieldTypes.SELECT,
               values: ['yes', 'no'],
-              validateFunction: Utils.alwaysTrue,
+              validateFunction: Utils.notNullValue,
             },
           ]}
           submitFunction={this.addExamBuilder}
